0900cdfe0d70963874865f666b608af390bee3bb
afoim <sudo@onani.cn>
2025-04-30 19:49:07 +0800

    将bio的句号改为感叹号

--- src/config.ts
+++ src/config.ts
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=5", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "AcoFork",
-	bio: "Protect What You Love.",
+	bio: "Protect What You Love!",
 	links: [
 		{
 			name: "Twitter",
