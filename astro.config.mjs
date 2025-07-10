ae9c5534933f2cb59a1d414bd68af6086f85c828
afoim <acofork@qq.com>
2025-07-11 04:14:43 +0800

    fix: 更新网站域名配置为包含www前缀

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://afo.im",
+	site: "https://www.afo.im",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
